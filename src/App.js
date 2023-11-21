2a4c2ea9103849b3a8581d0cdd066c7a77fd03d1
Edwin Alzate <EdwinAlzateA@>
2023-11-21 18:19:45 -0500

    Commit componente introduccion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Componentes/Header/Header";
+import Introduccion from "./Componentes/Introduccion/Introduccion";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Introduccion />
       </header>
     </div>
   );
--- src/Componentes/Introduccion/Introduccion.jsx
+++ src/Componentes/Introduccion/Introduccion.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Introduccion.css';
 
 function Introduccion() {
   return (
