fa912e8cc63f73cb02c8192e9c59e1c97a3c8746
Edwin Alzate <EdwinAlzateA@>
2023-11-21 21:59:44 -0500

    Commit con footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import "./App.css";
 import Header from "./Componentes/Header/Header";
 import Introduccion from "./Componentes/Introduccion/Introduccion";
+import Footer from "./Componentes/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <Introduccion />
       </header>
+      <main>
+        <Introduccion />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
-
 export default App;
---
+++ src/Componentes/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+import gato from "../../Imagenes/gato.png";
+import tienda from "../../Imagenes/tienda.jpg";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div id="imagenFooter">
+        <div id="imagenInternaGato">
+          <img src={gato} alt="footerGato" />
+        </div>
+      </div>
+      <p id="textoFooter">Tienda virtual de mascotas</p>
+      <p id="textoFacebook">Facebook: Tienda virtual de mascostas</p>
+      <p id="textoInstagram">Instagram: Tiendavirtualdemascotas</p>
+      <p id="textoTwitter">Twitter: @Tiendavirtualdemascotas</p>
+    </footer>
+  );
+}
+export default Footer;
